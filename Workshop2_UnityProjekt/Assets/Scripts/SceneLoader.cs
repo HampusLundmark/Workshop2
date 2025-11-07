e240bb2511fd9e894efabe65f93515c3b4368721
SnorlaxTjo <casmar1202@icloud.com>
2025-11-07 14:14:26 +0100

    Started on menu

--- Workshop2_UnityProjekt/Assets/Scripts/SceneLoader.cs
+++ Workshop2_UnityProjekt/Assets/Scripts/SceneLoader.cs
@@ -3,10 +3,30 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    private void ReloadScene()
+    public void LoadScene(int buildIndex)
+    {
+        SceneManager.LoadScene(LoopBuildIndex(buildIndex));
+    }
+    
+    private int LoopBuildIndex(int buildIndex)
+    {
+        if (buildIndex >= SceneManager.sceneCountInBuildSettings)
+        {
+            Debug.LogWarning("Build index out of range, loading scene 0");
+            buildIndex = 0;
+        }
+        return buildIndex;
+    }
+    
+    public void ReloadScene()
     {
         int sceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
     private void Update()
