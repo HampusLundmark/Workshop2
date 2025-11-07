afc0bf9741ed79c85e85352ecd11f3a122bcea25
Kraftware650 <raslev06@gmail.com>
2025-11-07 14:28:24 +0100

    script fix

--- Workshop2_UnityProjekt/Assets/Scripts/Rasmus/RasmusVariable.cs
+++ Workshop2_UnityProjekt/Assets/Scripts/Rasmus/RasmusVariable.cs
@@ -5,6 +5,5 @@ public class RasmusVariable : MonoBehaviour
     public int Var1 = 3;
     public int Var2 = 20;
     public int Var3 = 9;
-
-    [SerializeField] private int Var 4 = 69;
+    [SerializeField] private int Var4 = 32;
 }
