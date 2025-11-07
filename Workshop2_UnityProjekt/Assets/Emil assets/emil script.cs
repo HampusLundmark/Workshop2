41a1cdf2c4e55d1f3cc33d1844497bf116fe1e2e
BlooHoodDood <06emilhed@gmail.com>
2025-11-07 14:36:53 +0100

    mitt script funkar inte varför hjälåop

--- Workshop2_UnityProjekt/Assets/Emil assets/emil script.cs
+++ Workshop2_UnityProjekt/Assets/Emil assets/emil script.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class emil_script : MonoBehaviour
 {
     public float coolhet = 10.7f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
