5ddb91b72d5e58100e82e27c0869b33d3e1b843c
SnorlaxTjo <casmar1202@icloud.com>
2025-11-07 15:03:55 +0100

    I may have done something very evil

--- Workshop2_UnityProjekt/Assets/Scripts/CasperFun.cs
+++ Workshop2_UnityProjekt/Assets/Scripts/CasperFun.cs
@@ -1,7 +1,13 @@
+using System;
 using UnityEngine;
 
 public class CasperFun : MonoBehaviour
 {
     [SerializeField] private int amountOfBeans;
     [SerializeField] private int amountOfWater;
+
+    private void Start()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }
 }
